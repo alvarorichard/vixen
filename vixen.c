c98ba466ba06d06cfca9d38723730da3ba135820
alvarorichard <alvaro10048@protonmail.com>
2024-08-02 03:08:28 -0300

    add:rawmode

--- vixen.c
+++ vixen.c
@@ -1,9 +1,22 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <termios.h>
 
+void enableRawMode()
+{
+    struct termios raw;
+    
+    tcgetattr(STDIN_FILENO, &raw);
+    
+    raw.c_lflag &= ~(ECHO | ICANON);
+    
+    tcsetattr(STDIN_FILENO, TCSAFLUSH,&raw);
+}
 
 int main(int argc, char *argv[])
 {
+  enableRawMode();
+  
   char c;
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
   return EXIT_SUCCESS;
