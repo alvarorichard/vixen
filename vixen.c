0a05ec0aaddb35282b8fcfc912d64ef6c0397dc7
alvarorichard <alvaro10048@protonmail.com>
2024-08-02 04:02:44 -0300

    error handling

--- vixen.c
+++ vixen.c
@@ -1,14 +1,44 @@
+#include <stdio.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <termios.h>
+#include <ctype.h>
+#include <errno.h>
+
+struct termios orig_termios;
+
+void die(const char *s)
+{
+    perror(s);
+    exit(EXIT_FAILURE);
+}
+
+
+
+void disableRawMode()
+{
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
+}
+
 
 void enableRawMode()
 {
-    struct termios raw;
+   // struct termios raw;
+   
+    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+    atexit(disableRawMode);   
+    //tcgetattr(STDIN_FILENO, &raw);
+    struct termios raw = orig_termios;
+    raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+    raw.c_iflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
     
-    tcgetattr(STDIN_FILENO, &raw);
-    
-    raw.c_lflag &= ~(ECHO | ICANON);
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
     
     tcsetattr(STDIN_FILENO, TCSAFLUSH,&raw);
 }
@@ -17,7 +47,15 @@ int main(int argc, char *argv[])
 {
   enableRawMode();
   
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (1) {
+    char c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+    if (c == 'q') break;
+  }
   return EXIT_SUCCESS;
 }
