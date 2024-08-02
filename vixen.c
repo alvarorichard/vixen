4319be8c4862b19ee42353332b4ccc7e9cc3c6b2
alvarorichard <alvaro10048@protonmail.com>
2024-08-02 02:35:13 -0300

    add: gitiginore

--- vixen.c
+++ vixen.c
@@ -1,4 +1,10 @@
+#include <unistd.h>
 
 #define EXIT_SUCCESS 0
 
-int main(int argc, char *argv[]) { return EXIT_SUCCESS; }
+int main(int argc, char *argv[])
+{
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1);
+  return EXIT_SUCCESS;
+}
